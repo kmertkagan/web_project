51f109d7a8ab5a559bed49c9005aabf4beee89ee
Mert Kağan Kışla <kmertkagan@gmail.com>
2024-12-26 00:00:48 +0300

    add: contact.php

---
+++ contact.php
@@ -0,0 +1,17 @@
+
+
+<!-- Head -->
+<?php include "view/head.html" ?>
+<!-- End -->
+<body>    
+<!-- Header -->
+<?php include "view/header.php" ?>
+<!-- End -->
+
+<div class="logo-wrapper">
+    <div class="leftshadow"><img src="view/images/logo-wrap-left.jpg" /></div>
+        <div class="logo"><br><br>
+
+        </div>
+    </div>
+</div>

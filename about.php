6b38a92398089122bbea886e5d47a1cd76d35b5f
Mert Kağan Kışla <kmertkagan@gmail.com>
2024-12-25 23:51:46 +0300

    feat: explains how the app works

---
+++ about.php
@@ -0,0 +1,16 @@
+<!-- Head -->
+<?php include "view/head.html" ?>
+<!-- End -->
+<body>    
+<!-- Header -->
+<?php include "view/header.php" ?>
+<!-- End -->
+<div class="logo-wrapper">
+    <div class="leftshadow"><img src="view/images/logo-wrap-left.jpg" /></div>
+        <div class="logo"><br><br>
+        <p>Basit bir blog yazma ve görüntüleme uygulamasıdır. Veri tabanı tasarımı ise şu şekildedir</p><br>
+        <img src="./model/Screenshot 2024-12-21 031429.png" alt="Veritabanı">
+        <p hidden>Mert Kağan Kışla</p>
+        </div>
+    </div>
+</div>

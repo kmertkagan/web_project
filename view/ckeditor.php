33220518657c1bb69801f17182618d4e46f23748
Mert Kağan Kışla <kmertkagan@gmail.com>
2025-04-27 02:58:13 +0300

    feat: set view ckeditor.php file for the new feauture

--- view/ckeditor.php
+++ view/ckeditor.php
@@ -43,19 +43,6 @@
 		<option value="Moda">Moda</option>	
     	<option value="Başka Bir Konu">Başka Bir Konu</option>
   	</select><br><br><br>
-	<label id="uname">Kullancı Adı*</label><br>
-	<p id="uyari1" hidden>Zorunlu<p>
-	<input class="contact-input" type="text" name="username" required minlength="8"><br>
-	<label>İsim</label><br>
-	<input class="contact-input" type="text" name="name"><br>
-	<label>Soyisim</label><br>
-	<input class="contact-input" type="text" name="surname"><br>
-	<label>Yaş</label><br>
-	<input class="contact-input" type="number" name="age" min="18"><br>
-	<label>Cinsiyet</label> <br><br>
-	<input type="radio" name="gender" value="1"> Erkek 
-	<input type="radio" name="gender" value="0"> Kadın <br><br>
-	<label>Robot olmadığınızı kanıtlamak için görseldeki metni yazın</label><br>
 	<img src="controller/captcha.php" id="capt" onclick="return false;">
     <input type=button style="background: #2c3847;color: #ffffff" onClick=reload(); value='Tekrar Yükle'><br><br>
 	<input type="text" id="cap_value" name="cap_value" required><br><br>
